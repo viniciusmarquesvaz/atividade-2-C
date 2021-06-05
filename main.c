2c3beb43d8b9d7f1bdfbb21f63fe0ae8dbdc5f8a
viniciusmarquesvaz <80642632+viniciusmarquesvaz@users.noreply.github.com>
2021-06-05 14:01:14 -0300

    final update
    I made some changes inside the code.

--- main.c
+++ main.c
@@ -1,20 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <math.h>
 
 
 
-
-int main() {
-	float n;
-	float pi=3.1415;
-	
+int main()
+{
+	int num;    // número de coberturas a ser analizada 
+	float r; // raio da cobertura do circulo
+	float area; // area final
+	float menor;
+	float menorR;
+	int i; // variável de contagem
+	float j;
 	setlocale(LC_ALL,"Portuguese");
+
+    	
+    printf("\t informe quantas coberturas de área circular deseja verificar\n");
+    scanf("%d",&num);
+    
+    for(i=0;i<num;i++){
+    printf("\t informe o valor do raio do circulo:\n");
+    scanf("%f",&r);
+   	j=acos(-1); //operação inversa do coseno
+    area= j*(r*r);
+	if(i==0)
+	{
+		menor = area;
+		menorR = r;
+	}
+	else
+	{
+		if(area < menor)
+	{
+		menor=area;
+		menorR = r;
+	}
+	}
+	system("cls");
 	
-	printf("informe a quantidade de vezes que ser� calculado a �rea\n");
-	scanf("%f",&n);
 	
-	
-	
+	}
+	printf("a menor cobertura é a de: %f\nCorrespondente ao raio: %2.f\n",menor,menorR);
+    system("pause");
+    
+
+
+    
 	return 0;
 }
