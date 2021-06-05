fde8833835b77d9bb0a234f87f9322433e73c225
viniciusmarquesvaz <80642632+viniciusmarquesvaz@users.noreply.github.com>
2021-06-05 13:57:00 -0300

    Atividade avaliativa
    linguagem C

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+
+
+
+int main() {
+	float n;
+	float pi=3.1415;
+	
+	setlocale(LC_ALL,"Portuguese");
+	
+	printf("informe a quantidade de vezes que será calculado a área\n");
+	scanf("%f",&n);
+	
+	
+	
+	return 0;
+}
